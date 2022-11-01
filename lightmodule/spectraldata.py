1765d9baae46c96b646c941bb8ff9acace9aa256
Elmark <elmarkcorpus@ymail.com>
2022-11-01 10:55:50 +0800

    asdasdas

--- lightmodule/spectraldata.py
+++ lightmodule/spectraldata.py
@@ -524,7 +524,8 @@ def main():
     # Uncomment to conduct light controlling tests
     # LIGHT_CONTROL_TEST(PANEL_TOP) # First light control test
     # LIGHT_CONTROL_TEST_2(PANEL_TOP) # Second light control test
-    LIGHT_CONTROL_TEST_3(PANEL_TOP) # Third light control test
+    # LIGHT_CONTROL_TEST_3(PANEL_TOP) # Third light control test
+    # RAW_PCA9685_TEST() # Raw testing for PCA9685 controller
 
     # Uncomment to conduct light monitoring tests
     # LIGHT_MONITORING_TEST(SENSOR_ONE, SENSOR_TWO)
